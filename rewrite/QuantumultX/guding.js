926b3fa0dd6055ba35b49e43976b39e642ba8549
pxx917144686 <111606963+pxx917144686@users.noreply.github.com>
2024-07-07 10:31:12 +0800

    Update guding.js

--- rewrite/QuantumultX/guding.js
+++ rewrite/QuantumultX/guding.js
@@ -3,8 +3,8 @@
 站在巨人的肩膀上～抄袭  @pxx
 
 [rewrite_local]
-^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-response-body https://github.com/pxx917144686/ios/new/master/rewrite/QuantumultX/guding.js
-//^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-request-header https://github.com/pxx917144686/ios/new/master/rewrite/QuantumultX/guding.js
+^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-response-body https://raw.githubusercontent.com/pxx917144686/ios/master/rewrite/QuantumultX/guding.js
+//^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-request-header https://raw.githubusercontent.com/pxx917144686/ios/master/rewrite/QuantumultX/guding.js
 
 [MITM]
 hostname = api.revenuecat.com
