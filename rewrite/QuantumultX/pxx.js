e56b151484f5935be5b4eefa74ea62e0d3d96c4e
pxx917144686 <111606963+pxx917144686@users.noreply.github.com>
2024-08-06 12:56:20 +0800

    Update pxx.list

--- rewrite/QuantumultX/pxx.js
+++ rewrite/QuantumultX/pxx.js
@@ -1,28 +1,28 @@
 [policy]
-static=Bank, policy-path=DIRECT, BankRules
+static=兴业银行, policy-path=DIRECT, BankRules
 
 [filter_local]
-DOMAIN-SUFFIX,bank-of-china.com,Bank
-DOMAIN-SUFFIX,bankofchina.com,Bank
-DOMAIN-SUFFIX,boc.cn,Bank
-DOMAIN-SUFFIX,bocamchina.com,Bank
-DOMAIN-SUFFIX,bocaviation.com,Bank
-DOMAIN-SUFFIX,boccfc.CC,Bank
-DOMAIN-SUFFIX,boccfc.cn,Bank
-DOMAIN-SUFFIX,bocfullertonbank.com,Bank
-DOMAIN-SUFFIX,bocgi.com,Bank
-DOMAIN-SUFFIX,bocgins.com,Bank
-DOMAIN-SUFFIX,bochk.com,Bank
-DOMAIN-SUFFIX,bochkonline.com,Bank
-DOMAIN-SUFFIX,bocichina.cn,Bank
-DOMAIN-SUFFIX,bocichina.com,Bank
-DOMAIN-SUFFIX,bocichina.com.cn,Bank
-DOMAIN-SUFFIX,bocifunds.cn,Bank
-DOMAIN-SUFFIX,bocifunds.com,Bank
-DOMAIN-SUFFIX,bocifunds.com.cn,Bank
-DOMAIN-SUFFIX,bocigroup.com,Bank
-DOMAIN-SUFFIX,bocim.com,Bank
-DOMAIN-SUFFIX,bocservices.com.cn,Bank
-DOMAIN-SUFFIX,ftcwifi.com,Bank
+DOMAIN-SUFFIX,bank-of-china.com,兴业银行
+DOMAIN-SUFFIX,bankofchina.com,兴业银行
+DOMAIN-SUFFIX,boc.cn,兴业银行
+DOMAIN-SUFFIX,bocamchina.com,兴业银行
+DOMAIN-SUFFIX,bocaviation.com,兴业银行
+DOMAIN-SUFFIX,boccfc.CC,兴业银行
+DOMAIN-SUFFIX,boccfc.cn,兴业银行
+DOMAIN-SUFFIX,bocfullertonbank.com,兴业银行
+DOMAIN-SUFFIX,bocgi.com,兴业银行
+DOMAIN-SUFFIX,bocgins.com,兴业银行
+DOMAIN-SUFFIX,bochk.com,兴业银行
+DOMAIN-SUFFIX,bochkonline.com,兴业银行
+DOMAIN-SUFFIX,bocichina.cn,兴业银行
+DOMAIN-SUFFIX,bocichina.com,兴业银行
+DOMAIN-SUFFIX,bocichina.com.cn,兴业银行
+DOMAIN-SUFFIX,bocifunds.cn,兴业银行
+DOMAIN-SUFFIX,bocifunds.com,兴业银行
+DOMAIN-SUFFIX,bocifunds.com.cn,兴业银行
+DOMAIN-SUFFIX,bocigroup.com,兴业银行
+DOMAIN-SUFFIX,bocim.com,兴业银行
+DOMAIN-SUFFIX,bocservices.com.cn,兴业银行
+DOMAIN-SUFFIX,ftcwifi.com,兴业银行
 
 FINAL,DIRECT
