de8ee20d4a06c9ebaa02b2edea5ddbfaf40fc7a4
pxx917144686 <111606963+pxx917144686@users.noreply.github.com>
2024-08-14 01:25:29 +0800

    Update MG.js

--- rewrite/QuantumultX/MG.js
+++ rewrite/QuantumultX/MG.js
@@ -4,10 +4,6 @@
 适配了Mac版本iPad版本 
 脚本仅供学习和个人使用，不得用于商业目的或其他非法用途
 
-https://raw.githubusercontent.com/RuCu6/QuanX/main/Rewrites/Cube/cnftp.snippet
-感谢@RuCu6
-
-
 [rewrite_local]
 ^http[s]?:\/\/mobile\.api\.mgtv\.com\/v[0-9]\/(playlist|video\/album|video\/relative|video\/list).*$ url script-request-header https://raw.githubusercontent.com/pxx917144686/ios/master/rewrite/QuantumultX/MG1.js
 https://mobile-stream.api.mgtv.com/v1/video/source? url script-request-header https://raw.githubusercontent.com/pxx917144686/ios/master/rewrite/QuantumultX/MG.js
