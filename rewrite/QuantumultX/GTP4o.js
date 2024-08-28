adc929e5c7a53187226f5dd525186ded8dc6b5a9
pxx917144686 <111606963+pxx917144686@users.noreply.github.com>
2024-08-28 16:23:42 +0800

    Update GTP4o.js

--- rewrite/QuantumultX/GTP4o.js
+++ rewrite/QuantumultX/GTP4o.js
@@ -1,14 +1,15 @@
 /*
-GTP4o 以及 HD版   基于  YU9191 的数据
+GTP4o 
+
 [rewrite_local]
+
 https://nichousha.sjrjyffs.top/api/img/aiSketch url script-request-body https://raw.githubusercontent.com/pxx917144686/ios/master/rewrite/QuantumultX/GTP4ohd.js
 https://nichousha.sjrjyffs.top/api/app/user/getCurrentInfo url script-response-body https://raw.githubusercontent.com/pxx917144686/ios/master/rewrite/QuantumultX/GTP4o.js
 
 [mitm]
 hostname = nichousha.sjrjyffs.top
-
-脚本编辑人: pxx917144686
 */
+
 if ($request.url.includes('api/app/user/getCurrentInfo')) {
     let body = JSON.parse($response.body);
     Object.assign(body.data, { vipStatus: "1", nickname: "Baby", vipLabel: "1", imgNum: 9999 });
