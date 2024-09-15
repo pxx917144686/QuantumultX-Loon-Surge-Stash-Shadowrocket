f96bd6f61d8d557c35937e718e25b9531feebe28
pxx917144686 <111606963+pxx917144686@users.noreply.github.com>
2024-09-15 18:42:15 +0800

    Update HHH.js

--- rewrite/QuantumultX/HHH.js
+++ rewrite/QuantumultX/HHH.js
@@ -12,8 +12,8 @@ hostname = apiv2.hula123.com
 */
 let response = JSON.parse($response.body);
 Object.assign(response.data, {
-  vip_expire_at: "2066-06-06",
-  vip_expire_date: "2066-06-06",
+  vip_expire_at: "2099-09-09",
+  vip_expire_date: "2099-09-09",
   show_free_vip_dialog: true,
   is_free_vip: true,
   is_vip: true,
