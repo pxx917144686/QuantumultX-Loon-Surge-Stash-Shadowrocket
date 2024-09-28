6a377c54d6ba65633ee4bb6b8a72963b19996fe0
pxx917144686 <111606963+pxx917144686@users.noreply.github.com>
2024-09-29 04:38:15 +0800

    Create xinzhiweixinditu.js

---
+++ rewrite/QuantumultX/xinzhiweixinditu.js
@@ -0,0 +1,30 @@
+/*
+ * 新知卫星地图  需要登录 破解有效    @ pxx917144686
+
+
+
+[rewrite_local]
+^https:\/\/a\.xinzhi\.space\/api\/User\/getUserByToken url script-response-body https://raw.githubusercontent.com/afengye/QX/main/xinzhiweixinditu.js
+[mitm] 
+hostname = a.xinzhi.space
+*******************************/
+
+var aFengYe = $response.body;
+var obj =  JSON.parse(aFengYe);
+
+var vipInfo = {
+  "vip_type":"永久会员",
+  "vip_expire": "2066-06-06 00:00:00",
+  "is_vip": 1,
+  "vip_day": 66666
+}
+
+for (let key in obj.data) {
+  if (vipInfo.hasOwnProperty(key)) {
+     obj.data[key] = vipInfo[key]
+  }
+}
+
+
+aFengYe = JSON.stringify(obj);
+$done(aFengYe);
