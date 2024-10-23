11adb29243c088cd5f9d29f17b439ee8c75d8b04
pxx917144686 <111606963+pxx917144686@users.noreply.github.com>
2024-10-24 05:44:38 +0800

    Update VidHub.js

--- rewrite/QuantumultX/VidHub.js
+++ rewrite/QuantumultX/VidHub.js
@@ -1,14 +1,14 @@
 /*
- * VidHub 配置拦截脚本
+ * VidHub 配置拦截
 [rewrite_local]
-https://api.7littlemen.com/path/to/settings url script-response-body https://raw.githubusercontent.com/pxx917144686/ios/master/rewrite/QuantumultX/VidHub.js
-
+https://api.7littlemen.com/Vidhub/native_config.json url script-response-body https://raw.githubusercontent.com/pxx917144686/ios/master/rewrite/QuantumultX/VidHub.js
 */
 
 var body = $response.body;
 let obj = JSON.parse(body);
 
-// 修改API返回的用户支付状态
-obj.data["IsOneTimePayment"] = true; 
+// 修改API返回的配置，设置为一次性支付
+obj.data["IsOneTimePayment"] = true;
+
 body = JSON.stringify(obj);
 $done({body});
