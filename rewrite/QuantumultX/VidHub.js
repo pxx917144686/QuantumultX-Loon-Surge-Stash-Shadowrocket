0708fe0b0bab3cfebec0959f057786cfade37b46
pxx917144686 <111606963+pxx917144686@users.noreply.github.com>
2024-10-24 05:25:00 +0800

    Create VidHub.js

---
+++ rewrite/QuantumultX/VidHub.js
@@ -0,0 +1,14 @@
+/*
+ * VidHub 配置拦截脚本
+[rewrite_local]
+https://apiv2.hula123.com/api/v1/me url script-response-body https://raw.githubusercontent.com/pxx917144686/ios/master/rewrite/QuantumultX/VidHub.js
+
+*/
+
+var body = $response.body;
+let obj = JSON.parse(body);
+
+// 修改API返回的用户支付状态
+obj.data["IsOneTimePayment"] = true; 
+body = JSON.stringify(obj);
+$done({body});
