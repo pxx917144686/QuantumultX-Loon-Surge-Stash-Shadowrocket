310e98dfdefa2c48f3ed5cba8cbccf69fed10c19
pxx917144686 <111606963+pxx917144686@users.noreply.github.com>
2024-12-12 18:32:40 +0800

    Update Guding-daimengxiong.js

--- rewrite/Loon/Guding-daimengxiong.js
+++ rewrite/Loon/Guding-daimengxiong.js
@@ -1,7 +1,14 @@
-# Loon 配置脚本 //呆萌熊APP 容器 @boqii
-
+#!name = 呆萌熊AP 容器 @boqii
+#!desc = 不清楚~
+#!openUrl = https://apps.apple.com/app/id1360827510
+#!author = boqii[https://t.me/Guding88]
+#!tag = VIP
+#!system = 
+#!system_version = 
+#!homepage = https://github.com/Guding88/Script
+#!icon = https://github.com/pxx917144686/QuantumultX-Loon-Surge-Stash-Shadowrocket/blob/master/rewrite/Loon/%E9%AA%A8%E9%92%89.jpeg
 [Script]
-http-response ^https:\/\/appss\.rhinox\.cn\/ script-path=https://raw.githubusercontent.com/pxx917144686/ios/master/rewrite/QuantumultX/Guding-daimengxiong.js, requires-body=true, tag=呆萌熊VIP解锁
+http-response ^https:\/\/appss\.rhinox\.cn\/ script-path=https://raw.githubusercontent.com/afengye/QX/main/daimengxiong.js, requires-body=true, tag=呆萌熊VIP解锁
 
 [MITM]
 hostname = appss.rhinox.cn
