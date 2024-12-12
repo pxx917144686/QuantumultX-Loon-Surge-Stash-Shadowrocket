472b5de9dbb480c57adc2a0abe2e86a2b1e40d82
pxx917144686 <111606963+pxx917144686@users.noreply.github.com>
2024-12-12 18:21:43 +0800

    Create Guding-daimengxiong.js

---
+++ rewrite/Loon/Guding-daimengxiong.js
@@ -0,0 +1,7 @@
+# Loon 配置脚本 //呆萌熊APP 容器 @boqii
+
+[Script]
+http-response ^https:\/\/appss\.rhinox\.cn\/ script-path=https://raw.githubusercontent.com/pxx917144686/ios/master/rewrite/QuantumultX/Guding-daimengxiong.js, requires-body=true, tag=呆萌熊VIP解锁
+
+[MITM]
+hostname = appss.rhinox.cn
