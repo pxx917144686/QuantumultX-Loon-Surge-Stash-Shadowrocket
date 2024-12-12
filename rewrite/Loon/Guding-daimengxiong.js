642e4088c0cd654d2c914aabb1ffb6d6105592bc
pxx917144686 <111606963+pxx917144686@users.noreply.github.com>
2024-12-12 18:39:45 +0800

    Update Guding-daimengxiong.js

--- rewrite/Loon/Guding-daimengxiong.js
+++ rewrite/Loon/Guding-daimengxiong.js
@@ -6,7 +6,7 @@
 #!system = 
 #!system_version = 
 #!homepage = https://github.com/Guding88/Script
-#!icon = https://github.com/pxx917144686/QuantumultX-Loon-Surge-Stash-Shadowrocket/blob/master/rewrite/Loon/%E9%AA%A8%E9%92%89.jpeg
+#!icon = https://raw.githubusercontent.com/pxx917144686/QuantumultX-Loon-Surge-Stash-Shadowrocket/master/rewrite/Loon/%E9%AA%A8%E9%92%89.jpeg
 [Script]
 http-response ^https:\/\/appss\.rhinox\.cn\/ script-path=https://raw.githubusercontent.com/afengye/QX/main/daimengxiong.js, requires-body=true, tag=呆萌熊VIP解锁
 
