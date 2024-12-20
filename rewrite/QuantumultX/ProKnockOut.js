adb11c45cbcec8465b5f6c0477c51f7496bbeaed
pxx917144686 <111606963+pxx917144686@users.noreply.github.com>
2024-12-21 01:43:15 +0800

    Update ProKnockOut.js

--- rewrite/QuantumultX/ProKnockOut.js
+++ rewrite/QuantumultX/ProKnockOut.js
@@ -8,47 +8,52 @@ hostname = buy.itunes.apple.com
 
 const obj = JSON.parse($response.body);
 const { url, headers } = $request;
-const name = "ProKnockOut"; // App
-const productName = "com.loveyouchenapps.knockout"; // 产品ID
-const productType = "com.knockout.SVIP.50off"; // 类型
-const appVersion = "5"; // 应用版本
+const name = "ProKnockOut";
+const productName = "com.loveyouchenapps.knockout";
+const productType = "com.knockout.SVIP.50off";
+const appVersion = "5";
 
-// 判断 URL 是否为 "verifyReceipt" 请求，并且 User-Agent 是否包含 "ProKnockOut"
 if (/^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt/.test(url) && headers["User-Agent"].includes(name)) {
-    // 假响应数据
-    obj.status = "0";
-    obj.receipt_data = {
-        "status": 0,
-        "environment": "Production",
-        "receipt": {
-            "receipt_type": "Production",
-            "bundle_id": productName,
-            "original_purchase_date": "2023-08-14 15:27:40 Etc/GMT",
-            "in_app": [{
-                "quantity": "1",
-                "transaction_id": "666666666666667",
-                "original_transaction_id": "666666666666667",
-                "product_id": productType,
-                "purchase_date": "2023-08-14 15:27:40 Etc/GMT",
-                "expires_date": "2222-02-02 02:02:02 Etc/GMT"
-            }]
-        },
-        "latest_receipt_info": [{
-            "quantity": "1",
-            "transaction_id": "666666666666667",
-            "original_transaction_id": "666666666666667",
-            "product_id": productType,
-            "purchase_date": "2023-08-14 15:27:40 Etc/GMT",
-            "expires_date": "2222-02-02 02:02:02 Etc/GMT"
+    const receipt = {
+        receipt_type: "Production",
+        bundle_id: productName,
+        in_app: [{
+            quantity: "1",
+            transaction_id: "666666666666667",
+            original_transaction_id: "666666666666667",
+            product_id: productType,
+            in_app_ownership_type: "PURCHASED",
+            purchase_date: "2023-08-14 15:27:40 Etc/GMT",
+            purchase_date_ms: "1691972860000",
+            expires_date: "2222-02-02 02:02:02 Etc/GMT",
+            expires_date_ms: "7955085722000",
         }],
-        "pending_renewal_info": [{
-            "product_id": productName,
-            "original_transaction_id": "666666666666667",
-            "auto_renew_product_id": productType,
-            "auto_renew_status": "1"
-        }]
+        adam_id: 1111111111,
+        receipt_creation_date_pst: "2023-08-14 08:25:04 America/Los_Angeles",
+        application_version: appVersion,
+        latest_receipt_info: [{
+            quantity: "1",
+            transaction_id: "666666666666667",
+            original_transaction_id: "666666666666667",
+            product_id: productType,
+            in_app_ownership_type: "PURCHASED",
+            expires_date: "2222-02-02 02:02:02 Etc/GMT",
+            expires_date_ms: "7955085722000",
+        }],
+        pending_renewal_info: [{
+            product_id: productName,
+            original_transaction_id: "666666666666667",
+            auto_renew_product_id: productType,
+            auto_renew_status: "1",
+        }],
+        status: 0,
+        environment: "Production",
     };
+
+    obj.latest_receipt_info = receipt.latest_receipt_info;
+    obj.latest_receipt = "";
+    obj.pending_renewal_info = receipt.pending_renewal_info;
+    obj.receipt = receipt;
 }
 
-// 返回修改后的响应
 $done({ body: JSON.stringify(obj) });
