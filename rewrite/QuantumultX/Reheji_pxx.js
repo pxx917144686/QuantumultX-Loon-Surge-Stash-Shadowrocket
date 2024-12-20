6ee59143f5b07a2661b3dc3e45a42923e6d0f3a3
pxx917144686 <111606963+pxx917144686@users.noreply.github.com>
2024-12-21 05:28:50 +0800

    Update Reheji_pxx.js

--- rewrite/QuantumultX/Reheji_pxx.js
+++ rewrite/QuantumultX/Reheji_pxx.js
@@ -5,7 +5,7 @@
 ^https:\/\/api\.(revenuecat|rc-backup)\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/pxx917144686/QuantumultX-Loon-Surge-Stash-Shadowrocket/refs/heads/master/rewrite/QuantumultX/Reheji_pxx.js
 ^https:\/\/api\.(revenuecat|rc-backup)\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/pxx917144686/QuantumultX-Loon-Surge-Stash-Shadowrocket/refs/heads/master/rewrite/QuantumultX/Reheji_pxx.js
 [mitm]
-hostname = api.rc-backup.com
+hostname = api.revenuecat.com, api.rc-backup.com
 *************************************/
 const obj = {};
 
