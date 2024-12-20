8566f1c77ce6a18b7d421b48521d83e232aef7c2
pxx917144686 <111606963+pxx917144686@users.noreply.github.com>
2024-12-21 02:02:19 +0800

    Update Boom.js

--- rewrite/QuantumultX/Boom.js
+++ rewrite/QuantumultX/Boom.js
@@ -5,60 +5,76 @@ Boom Apple商店:https://apps.apple.com/us/app/boom-bass-booster-equalizer/id106
 [MITM]
 hostname = apimboom2.globaldelight.net
 */
-
-const obj = JSON.parse($response.body);
-const { url, headers } = $request;
-
-// 判断
-const name = "Boom";  // 产品名称
-const productName = "com.globaldelight.iBoom";  // 产品ID
-const productType = "com.globaldelight.iBoom.LifetimeDiscountPack";  // 内购类型
-const appVersion = "4191";  // 应用版本
-
-// 如果请求符合,修改返回数据
-if (/^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt/.test(url) && headers["User-Agent"].includes(name)) {
-    const receipt = {
-        receipt_type: "Production",
-        bundle_id: productName,
-        in_app: [{
-            quantity: "1",
-            transaction_id: "470000445785125",
-            original_transaction_id: "470000445785125",
-            product_id: productType,
-            in_app_ownership_type: "PURCHASED",
-            purchase_date: "2019-04-17 04:10:56 Etc/GMT",
-            purchase_date_ms: "1555474256000",
-            expires_date: "2099-04-17 04:10:56 Etc/GMT",
-            expires_date_ms: "1587096656000",
+let obj = JSON.parse($response.body);
+obj = {
+    "status": "0",
+    "receipt-data": {
+        "status": 0,
+        "environment": "Production",
+        "receipt": {
+            "receipt_type": "Production",
+            "app_item_id": 1065511007,
+            "receipt_creation_date": "2019-10-30 16:52:23 Etc\\\\/GMT",
+            "bundle_id": "com.globaldelight.iBoom",
+            "original_purchase_date": "2019-04-17 04:07:39 Etc\\\\/GMT",
+            "in_app": [{
+                "quantity": "1",
+                "purchase_date_ms": "1555474256000",
+                "expires_date": "2099-04-17 04:10:56 Etc\\\\/GMT",
+                "expires_date_pst": "2099-04-16 21:10:56 America\\\\/Los_Angeles",
+                "is_in_intro_offer_period": "false",
+                "transaction_id": "470000445785125",
+                "is_trial_period": "false",
+                "original_transaction_id": "470000445785125",
+                "purchase_date": "2019-04-17 04:10:56 Etc\\\\/GMT",
+                "product_id": "com.globaldelight.iBoom.LifetimeDiscountPack",
+                "original_purchase_date_pst": "2019-04-16 21:10:59 America\\\\/Los_Angeles",
+                "original_purchase_date_ms": "1555474259000",
+                "web_order_line_item_id": "470000137081235",
+                "expires_date_ms": "1587096656000",
+                "purchase_date_pst": "2019-04-16 21:10:56 America\\\\/Los_Angeles",
+                "original_purchase_date": "2019-04-17 04:10:59 Etc\\\\/GMT"
+            }],
+            "adam_id": 1065511007,
+            "receipt_creation_date_pst": "2019-10-30 09:52:23 America\\\\/Los_Angeles",
+            "request_date": "2019-10-30 16:52:29 Etc\\\\/GMT",
+            "request_date_pst": "2019-10-30 09:52:29 America\\\\/Los_Angeles",
+            "version_external_identifier": 832251566,
+            "request_date_ms": "1572454349573",
+            "original_purchase_date_pst": "2019-04-16 21:07:39 America\\\\/Los_Angeles",
+            "application_version": "1.4.70002",
+            "original_purchase_date_ms": "1555474059000",
+            "receipt_creation_date_ms": "1572454343000",
+            "original_application_version": "1.4.10008",
+            "download_id": 87042883772350
+        },
+        "latest_receipt_info": [{
+            "quantity": "1",
+            "purchase_date_ms": "1555474256000",
+            "expires_date": "2099-04-17 04:10:56 Etc\\\\/GMT",
+            "expires_date_pst": "2099-04-16 21:10:56 America\\\\/Los_Angeles",
+            "is_in_intro_offer_period": "false",
+            "transaction_id": "470000445785125",
+            "is_trial_period": "false",
+            "original_transaction_id": "470000445785125",
+            "purchase_date": "2019-04-17 04:10:56 Etc\\\\/GMT",
+            "product_id": "com.globaldelight.iBoom.LifetimeDiscountPack",
+            "original_purchase_date_pst": "2019-04-16 21:10:59 America\\\\/Los_Angeles",
+            "subscription_group_identifier": "20461753",
+            "original_purchase_date_ms": "1555474259000",
+            "web_order_line_item_id": "470000137081235",
+            "expires_date_ms": "4080082256000",
+            "purchase_date_pst": "2019-04-16 21:10:56 America\\\\/Los_Angeles",
+            "original_purchase_date": "2019-04-17 04:10:59 Etc\\\\/GMT"
         }],
-        adam_id: 1065511007,
-        receipt_creation_date_pst: "2019-10-30 09:52:23 America/Los_Angeles",
-        application_version: appVersion,
-        latest_receipt_info: [{
-            quantity: "1",
-            transaction_id: "470000445785125",
-            original_transaction_id: "470000445785125",
-            product_id: productType,
-            in_app_ownership_type: "PURCHASED",
-            expires_date: "2099-04-17 04:10:56 Etc/GMT",
-            expires_date_ms: "1587096656000",
-        }],
-        pending_renewal_info: [{
-            product_id: productName,
-            original_transaction_id: "470000445785125",
-            auto_renew_product_id: productType,
-            auto_renew_status: "0",
-        }],
-        status: 0,
-        environment: "Production",
-    };
-
-    // 直接替换响应数据
-    obj.latest_receipt_info = receipt.latest_receipt_info;
-    obj.latest_receipt = "";
-    obj.pending_renewal_info = receipt.pending_renewal_info;
-    obj.receipt = receipt;
-}
-
-// 返回修改后的响应
-$done({ body: JSON.stringify(obj) });
+        "pending_renewal_info": [{
+            "product_id": "com.globaldelight.iBoom.LifetimeDiscountPack",
+            "original_transaction_id": "470000445785125",
+            "auto_renew_product_id": "com.globaldelight.iBoom.LifetimeDiscountPack",
+            "auto_renew_status": "0"
+        }]
+    }
+};
+$done({
+    body: JSON.stringify(obj)
+});
