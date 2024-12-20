54d4d459177fa37f77e59714cb35ac01b708e0f1
pxx917144686 <111606963+pxx917144686@users.noreply.github.com>
2024-12-21 01:36:30 +0800

    Update ProKnockOut.js

--- rewrite/QuantumultX/ProKnockOut.js
+++ rewrite/QuantumultX/ProKnockOut.js
@@ -6,54 +6,77 @@ https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https:
 hostname = buy.itunes.apple.com
 */
 
-const obj = JSON.parse($response.body);
-const { url, headers } = $request;
-const name = "ProKnockOut";
-const productName = "com.loveyouchenapps.knockout";
-const productType = "com.knockout.SVIP.50off";
-const appVersion = "5";
+let obj = JSON.parse($response.body);
 
-if (/^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt/.test(url) && headers["User-Agent"].includes(name)) {
-    const receipt = {
-        receipt_type: "Production",
-        bundle_id: productName,
-        in_app: [{
-            quantity: "1",
-            transaction_id: "666666666666667",
-            original_transaction_id: "666666666666667",
-            product_id: productType,
-            in_app_ownership_type: "PURCHASED",
-            purchase_date: "2023-08-14 15:27:40 Etc/GMT",
-            purchase_date_ms: "1691972860000",
-            expires_date: "2222-02-02 02:02:02 Etc/GMT",
-            expires_date_ms: "7955085722000",
+// 模拟内购凭证数据
+obj = {
+    "status": "0",
+    "receipt-data": {
+        "status": 0,
+        "environment": "Production",
+        "receipt": {
+            "receipt_type": "Production",
+            "app_item_id": 944665061,
+            "receipt_creation_date": "2023-08-14 15:27:40 Etc/GMT",
+            "bundle_id": "com.loveyouchenapps.knockout",
+            "original_purchase_date": "2023-08-14 15:27:40 Etc/GMT",
+            "in_app": [{
+                "quantity": "1",
+                "purchase_date_ms": "1691972860000",
+                "expires_date": "2222-02-02 02:02:02 Etc/GMT",
+                "expires_date_ms": "7955085722000",
+                "is_in_intro_offer_period": "false",
+                "transaction_id": "666666666666667",
+                "is_trial_period": "false",
+                "original_transaction_id": "666666666666667",
+                "purchase_date": "2023-08-14 15:27:40 Etc/GMT",
+                "product_id": "com.knockout.SVIP.50off",
+                "original_purchase_date_pst": "2023-08-14 08:27:40 America/Los_Angeles",
+                "original_purchase_date_ms": "1691972861000",
+                "web_order_line_item_id": "470000137081235",
+                "expires_date_ms": "7955085722000",
+                "purchase_date_pst": "2023-08-14 08:27:40 America/Los_Angeles",
+                "original_purchase_date": "2023-08-14 15:27:40 Etc/GMT"
+            }],
+            "adam_id": 944665061,
+            "receipt_creation_date_pst": "2023-08-14 08:27:40 America/Los_Angeles",
+            "request_date": "2023-08-14 15:27:40 Etc/GMT",
+            "request_date_pst": "2023-08-14 08:27:40 America/Los_Angeles",
+            "version_external_identifier": 832251566,
+            "request_date_ms": "1691972860000",
+            "original_purchase_date_pst": "2023-08-14 08:27:40 America/Los_Angeles",
+            "application_version": "5.0.0",
+            "original_purchase_date_ms": "1691972860000",
+            "receipt_creation_date_ms": "1691972860000",
+            "original_application_version": "5.0.0",
+            "download_id": 87042883772350
+        },
+        "latest_receipt_info": [{
+            "quantity": "1",
+            "purchase_date_ms": "1691972860000",
+            "expires_date": "2222-02-02 02:02:02 Etc/GMT",
+            "expires_date_pst": "2222-02-02 02:02:02 America/Los_Angeles",
+            "is_in_intro_offer_period": "false",
+            "transaction_id": "666666666666667",
+            "is_trial_period": "false",
+            "original_transaction_id": "666666666666667",
+            "purchase_date": "2023-08-14 15:27:40 Etc/GMT",
+            "product_id": "com.knockout.SVIP.50off",
+            "original_purchase_date_pst": "2023-08-14 08:27:40 America/Los_Angeles",
+            "subscription_group_identifier": "20461753",
+            "original_purchase_date_ms": "1691972861000",
+            "web_order_line_item_id": "470000137081235",
+            "expires_date_ms": "7955085722000",
+            "purchase_date_pst": "2023-08-14 08:27:40 America/Los_Angeles",
+            "original_purchase_date": "2023-08-14 15:27:40 Etc/GMT"
         }],
-        adam_id: 1111111111,
-        receipt_creation_date_pst: "2023-08-14 08:25:04 America/Los_Angeles",
-        application_version: appVersion,
-        latest_receipt_info: [{
-            quantity: "1",
-            transaction_id: "666666666666667",
-            original_transaction_id: "666666666666667",
-            product_id: productType,
-            in_app_ownership_type: "PURCHASED",
-            expires_date: "2222-02-02 02:02:02 Etc/GMT",
-            expires_date_ms: "7955085722000",
-        }],
-        pending_renewal_info: [{
-            product_id: productName,
-            original_transaction_id: "666666666666667",
-            auto_renew_product_id: productType,
-            auto_renew_status: "1",
-        }],
-        status: 0,
-        environment: "Production",
-    };
-
-    obj.latest_receipt_info = receipt.latest_receipt_info;
-    obj.latest_receipt = "";
-    obj.pending_renewal_info = receipt.pending_renewal_info;
-    obj.receipt = receipt;
-}
+        "pending_renewal_info": [{
+            "product_id": "com.loveyouchenapps.knockout",
+            "original_transaction_id": "666666666666667",
+            "auto_renew_product_id": "com.knockout.SVIP.50off",
+            "auto_renew_status": "1"
+        }]
+    }
+};
 
 $done({ body: JSON.stringify(obj) });
