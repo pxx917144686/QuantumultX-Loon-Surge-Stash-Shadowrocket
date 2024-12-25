9b0ca24d82bd53fc8960cf294ff9aaf2b57d26df
pxx917144686 <111606963+pxx917144686@users.noreply.github.com>
2024-12-25 16:26:14 +0800

    Update xunji.js

--- rewrite/QuantumultX/xunji.js
+++ rewrite/QuantumultX/xunji.js
@@ -2,7 +2,7 @@
 训记 
 [rewrite_local]
 #训记vip
-^https:\/\/api\.xunjiapp\.cn\/whole_user_info url script-response-body https://raw.githubusercontent.com/pxx917144686/ios/master/rewrite/QuantumultX/Xunji.js
+^https:\/\/api\.xunjiapp\.cn\/whole_user_info url script-response-body https://raw.githubusercontent.com/pxx917144686/ios/master/rewrite/QuantumultX/xunji.js
 [mitm]
 hostname = api.xunjiapp.cn
 */
